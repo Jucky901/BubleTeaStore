0c8be1a70d33314f7918e791c756888c9f1d74d4
Jucky901 <truongvuon235@gmail.com>
2025-05-31 23:20:14 +0700

    Able to host, Remember to .\mvnw.cmd clean install and .\mvnw.cmd spring-boot:run

---
+++ store/src/main/java/com/bubletea/store/HomeController.java
@@ -0,0 +1,12 @@
+package com.bubletea.store;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Bubble Tea!";
+    }
+}
